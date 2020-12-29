5b19d29c3e1bba5e47c7b12774c9eef89547fc9e
isaiah <greatpup1940@gmail.com>
2020-12-29 02:40:58 -0500

    Added particles background

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,18 @@
 import React, {useState, useEffect} from "react"
 import axios from "axios"
 import NavBar from './nav'
+import Particles from './particles'
 
 const Layout = ({ location, title, children }) => {
 
+
   return (
     <div>
+      <Particles />
+      <NavBar></NavBar>
       <div className="global-wrapper" >
         <main>{children}</main>
       </div>
-        <NavBar></NavBar>
     </div>
   )
 }
